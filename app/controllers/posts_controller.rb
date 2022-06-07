85c06053240494cabf33d4a61b0d61c94e127218
adambujKellogg <adam.bujnowski@kellogg.northwestern.edu>
2022-06-07 20:54:21 +0000

    Initial commits from Adam

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,24 @@
 class PostsController < ApplicationController
 
+  def index
+    @post = Post.where({ "user_id" => session["user_id"] })
+    @post = Post.new
+  end
+
   def new
     @post = Post.new
     @post.place_id = params["place_id"]
   end
 
   def create
+    @current_user = User.find_by({ "id" => session["user_id"]})
     @post = Post.new
     @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["posted_on"]
     @post["place_id"] = params["post"]["place_id"]
+    @post["user_id"] = session["user_id"]
+    @post.uploaded_image.attach(params["post"]["uploaded_image"])
     @post.save
     redirect_to "/places/#{@post["place_id"]}"
   end
